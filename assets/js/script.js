59f33627f37cfe5e6c9f9841bfabed599bc103da
saranganet <soham.saranga@adypu.edu.in>
2024-12-08 04:16:23 +0530

    I have added a book table page, in which added features to add, name, no. of people and confimation button, also improved the bottom contact us page, as the shape was not upto the mark.

--- assets/js/script.js
+++ assets/js/script.js
@@ -168,3 +168,19 @@ window.addEventListener("mousemove", function (event) {
   }
 
 });
+
+/**
+ * SMOOTH SCROLLING
+ */
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
